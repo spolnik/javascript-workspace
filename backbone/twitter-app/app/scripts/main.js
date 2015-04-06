a5eaec3d9dba25372c0cc6bfe7ed0766d3930738
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-04-06 14:27:01 +0200

    - yo backbone for Twitter-App

---
+++ backbone/twitter-app/app/scripts/main.js
@@ -0,0 +1,27 @@
+/*global require*/
+'use strict';
+
+require.config({
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        },
+        handlebars: {
+            exports: 'Handlebars'
+        }
+    },
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery',
+        backbone: '../bower_components/backbone/backbone',
+        underscore: '../bower_components/lodash/dist/lodash',
+        bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap',
+        handlebars: '../bower_components/handlebars/handlebars'
+    }
+});
+
+require([
+    'backbone'
+], function (Backbone) {
+    Backbone.history.start();
+});
