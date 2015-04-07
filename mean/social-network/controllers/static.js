60df3b8ec399e8c036ecb29f3d9312bc7e222e15
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-04-07 14:26:28 +0200

    - add support for ng router - add router for templates - extract posts template

--- mean/social-network/controllers/static.js
+++ mean/social-network/controllers/static.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.use(express.static(__dirname + '/../assets'));
+router.use(express.static(__dirname + '/../templates'));
 
 router.get('/', function (req, res) {
-    res.render('posts.html.ejs');
+    res.render('app.html.ejs');
 });
 
 module.exports = router;
