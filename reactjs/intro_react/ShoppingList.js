2db91edc00fb31bc12c37311d8f402e478718426
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-07-05 10:05:22 +0200

    make app more dynamic - adding quantity for items

--- reactjs/intro_react/ShoppingList.js
+++ reactjs/intro_react/ShoppingList.js
@@ -2,26 +2,67 @@ var priceToUSDString = function(price) {
     return '$' + price.toFixed(2);
 };
 
+var ShoppingItemQuantity = React.createClass({displayName: "ShoppingItemQuantity",
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+        onChange: React.PropTypes.func.isRequired
+    },
+
+    render: function () {
+        return React.createElement("div", null, React.createElement("input", {className: "spin-here"}))
+    },
+
+    spinnerInput: function() {
+        return $('.spin-here', this.getDOMNode());
+    },
+
+    componentDidMount: function() {
+        var spinnerInput = this.spinnerInput();
+        spinnerInput.spinner({
+            min: 0,
+            spin: this.props.onChange
+        });
+        spinnerInput.spinner('value', this.props.item.get('quantity'));
+        spinnerInput.on('change', this.props.onChange);
+    },
+
+    componentWillReceiveProps: function (nextProps) {
+        this.spinnerInput()
+            .spinner('value', nextProps.item.get('quantity'));
+    },
+
+    shouldComponentUpdate: function (nextProps) {
+        return false;
+    },
+
+    componentWillUnmount: function () {
+        this.spinnerInput().spinner('destroy');
+    }
+});
+
 var ShoppingItemRow = React.createClass({displayName: "ShoppingItemRow",
     propTypes: {
         item: React.PropTypes.object
     },
 
     render: function() {
-        var fields = [];
-        var value;
-
-        for (var fieldName in this.props.item) {
-            value = this.props.item[fieldName];
-            if (fieldName === 'price') {
-                value = priceToUSDString(value);
-            }
-            fields.push(React.createElement("li", {className: fieldName, key: fieldName}, value));
-        }
-
+        var item = this.props.item;
         return React.createElement("li", null, 
-            React.createElement("ul", null, fields)
+            React.createElement("ul", null, 
+                React.createElement("li", {className: "name"}, item.get('name')), 
+                React.createElement("li", {className: "quantity"}, 
+                    React.createElement(ShoppingItemQuantity, {item: item, 
+                                          onChange: this.onChangeQuantity})
+                ), 
+                React.createElement("li", {className: "price"}, 
+                    priceToUSDString(item.get('quantity') * item.get('price'))
+                )
+            )
         );
+    },
+
+    onChangeQuantity: function (event, ui) {
+        this.props.item.set('quantity', ui ? ui.value : event.target.value);
     }
 });
 
@@ -31,8 +72,8 @@ var ShoppingTotal = React.createClass({displayName: "ShoppingTotal",
     },
 
     render: function() {
-        var total = this.props.items.reduce(function(runningTotal, item) {
-            return item.price + runningTotal;
+        var total = this.props.collection.reduce(function(runningTotal, item) {
+            return (item.get('price') * item.get('quantity')) + runningTotal;
         }, 0);
 
         return React.createElement("ul", {className: "total"}, 
@@ -50,31 +91,11 @@ var ShoppingList = React.createClass({displayName: "ShoppingList",
     render: function() {
         return React.createElement("div", null, 
             React.createElement("ol", {className: "items"}, 
-                this.props.items.map(function(item) {
+                this.props.collection.map(function(item) {
                     return React.createElement(ShoppingItemRow, {item: item})
                 })
             ), 
-            React.createElement(ShoppingTotal, React.__spread({},  this.props))
+            React.createElement(ShoppingTotal, {collection: this.props.collection})
         );
     }
 });
-
-var itemList = [
-    {
-        name: 'Sleeping Bag w/ Stuff Sack',
-        quantity: 1,
-        price: 44.99
-    },
-    {
-        name: 'Chocolate Energy Bar',
-        quantity: 4,
-        price: 2.99 * 4
-    },
-    {
-        name: '2-Person Polyethylene Tent',
-        quantity: 1,
-        price: 104.33
-    }
-];
-
-React.render(React.createElement(ShoppingList, {items: itemList}), document.getElementById('here'));
